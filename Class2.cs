3110ad416afa1e0d6ec4c1c49af2495333e1acde
MDD <MDD@DESKTOP-BG4MK63>
2022-09-30 23:47:41 +0300

    Добавьте файлы проекта.

---
+++ Class2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_3
+{
+    class Class2
+    {
+        public static void Init(out double[,] matr, int column, int row)
+        {
+            Random Rnd; Rnd = new Random();
+            matr = new Double[row, column];
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    matr[i, j] = Math.Round(Rnd.Next(10)+ Rnd.NextDouble(),2);
+                }
+            }
+        }
+        
+        public static void Create(out double[,] matr, int column, int row)
+        {
+            matr = new double[row, column];
+        }
+        public static void Raschet(double[,] matr, out double summ)
+        {
+            int i, j;
+            summ = 0;
+            int kol = 0;
+            double sumcount=0,a=0;
+            for (j = 0; j < matr.GetLength(1); j++)
+            {
+                for (i = 0; i < matr.GetLength(0); i++)
+                {
+                    a = matr[i, j];
+                    sumcount += a;
+                    if (matr[i, j] == 0)
+                    {
+                        kol++;
+                    }
+                }
+                if (kol >= 1)
+                {
+                    summ += sumcount;
+                }
+                sumcount = 0;
+                kol =0;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практическая_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        double[,] matr;
+        private void dataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            int columnIndex = e.Column.DisplayIndex;
+            int rowIndex = e.Row.GetIndex();
+            matr[rowIndex, columnIndex] = Convert.ToInt32(((TextBox)e.EditingElement).Text);
+        }
+        
+        private void Заполнить_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(RowCount.Text, out int row) == true && Int32.TryParse(ColumnCount.Text, out int count) == true)
+            {
+                Class2.Init(out matr, count, row);
+                dataGrid.ItemsSource = Class1.ToDataTable(matr).DefaultView;
+            }
+            else MessageBox.Show("Введены некорректные значения", "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void Создать_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(RowCount.Text, out int row) == true && Int32.TryParse(ColumnCount.Text, out int count) == true)
+            {
+                Class2.Create(out matr, count, row);
+                dataGrid.ItemsSource = Class1.ToDataTable(matr).DefaultView;
+            }
+            else MessageBox.Show("Введены некорректные значения", "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        private void Raschet_Click(object sender, RoutedEventArgs e)
+        {
+            Class2.Raschet(matr, out double summ);
+            sum.Text = summ.ToString();
+
+        }
+        
+    }
+}
